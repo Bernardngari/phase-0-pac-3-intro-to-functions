a1a27eeae5de1549f155031abe2e8dccc1daf741
Bernardngari <bernardngari.ke@gmail.com>
2022-06-03 05:36:58 +0300

    commit tree 29c76927f18838ed8964874f4155ecfda0ce50a7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Follow along with the examples here
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
